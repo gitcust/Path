77ac0a3067296c5ffd280e724e5c121d5977a1be
gitcust <63855212+gitcust@users.noreply.github.com>
2020-04-17 16:31:47 +0200

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+npm install express
 import {PersonDatabase} from "./database/person-database";
 import {CompanyDatabase} from "./database/company-database";
 import {TaskDatabase} from "./database/task-database";
